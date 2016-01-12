7b2e69b98ebc7924c9cc699e8f7f7c69a64bdd1f
vynypm <vynypm19@hotmail.com>
2016-01-12 15:41:17 -0500

    Deberes

---
+++ bimestre2/deber12/ejercicio3/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Deber 12</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+	<h2>Copa Intercontinental</h2>
+	<div>
+		<table id="tabla" style="width:100%">
+			<tr class="verde">
+				<th>Equipo</th>
+				<th>Campeonatos</th>
+				<th>Subcampeonatos</th>
+			</tr>
+			<tr class="rojo">
+				<td>Milan</td>
+				<td>3</td>
+				<td>4</td>
+			</tr>
+			<tr class="verde">
+				<td>Real Madrid</td>
+				<td>3</td>
+				<td>2</td>
+			</tr>
+			<tr class="rojo">
+				<td>Peñarol</td>
+				<td>3</td>
+				<td>2</td>
+			</tr>
+			<tr class="verde">
+				<td>Boca Juniors</td>
+				<td>3</td>
+				<td>1</td>
+			</tr>
+			<tr class="rojo">
+				<td>Nacional(Uruguay)</td>
+				<td>3</td>
+				<td>0</td>
+			</tr>
+		</table>
+	</div>
+	<br>
+	<h3 id="mensage">"   "</h3>
+	<br>
+	<div id="focus">
+		<h2>Focus y blur</h2>
+		<input type="text" value="" placeholder="Pulse aquí">
+	</div>
+	<script src="js/jquery.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
